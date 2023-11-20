6b11be2ca7224400ced2d6551420c32007bcfe25
Matsonj30 <matsonj2013@gmail.com>
2023-11-19 18:19:17 -0700

    testimonial

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,42 @@ function scrollToDiv(location){
     document.getElementById(location).scrollIntoView({behavior: "smooth"});
 
 }
+
+
+function changeTestimonial(direction){
+    var testimonials = document.getElementsByClassName("testimonial")
+    var currentSpot = -1;
+    for(var i = 0; i < testimonials.length; i+=1){
+        if(window.getComputedStyle(testimonials[i]).display != 'none'){
+            testimonials[i].style.display = 'none'; 
+            currentSpot = i;
+        }
+    }
+    if((currentSpot + direction + 1) > testimonials.length){
+        testimonials[0].style.display = 'block';
+    }
+    else if((currentSpot + direction) < 0){
+        testimonials[1].style.display = 'block';
+    }
+    else{
+        testimonials[currentSpot + direction].style.display = 'block';
+    }
+}
+
+function handleResize() {
+    // Check the new window width
+    var currentWidth = window.innerWidth;
+    var testimonials = document.getElementsByClassName("testimonial")
+    // Perform actions based on the window width
+    if (currentWidth < 1300) {
+        for(var i = 0; i < testimonials.length; i+=1){
+            testimonials[i].style = 'block';
+        }
+    } 
+}
+
+// Attach the event listener to the window's resize event
+window.addEventListener('resize', handleResize);
+
+// Initial call to handleResize to handle the initial screen width
+handleResize();
